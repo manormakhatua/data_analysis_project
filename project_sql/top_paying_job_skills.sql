addb0aefb650d86a28e3ef49b3a9953a0802603f
Manorma Khatua <100094506+manormakhatua@users.noreply.github.com>
2024-07-20 21:50:06 +0530

    skills

---
+++ project_sql/top_paying_job_skills.sql
@@ -0,0 +1,385 @@
+/*
+Question: What skills are required for the top-paying data analyst jobs?
+- Use the top 10 highest-paying Data Analyst jobs from first query
+- Add the specific skills required for these roles
+- Why? It provides a detailed look at which high-paying jobs demand certain skills, 
+    helping job seekers understand which skills to develop that align with top salaries
+*/
+
+with top_paying_jobs AS(
+SELECT
+      job_id,
+      job_title,
+      salary_year_avg,
+      name as company_name
+FROM
+    job_postings_fact
+    LEFT JOIN
+    company_dim ON company_dim.company_id = job_postings_fact.company_id
+WHERE
+    job_title = 'Data Analyst' AND
+    job_location = 'Anywhere' AND
+    salary_year_avg is NOT NULL
+    order BY salary_year_avg DESC
+    limit 10)
+SELECT
+top_paying_jobs.*,
+skills
+FROM top_paying_jobs
+INNER JOIN skills_job_dim on skills_job_dim.job_id = top_paying_jobs.job_id
+LEFT JOIN skills_dim on skills_dim.skill_id = skills_job_dim.skill_id
+ORDER BY salary_year_avg DESC
+
+/*Top Skills for the Highest-Paying Data Analyst Roles:
+            
+skills	     count
+python	      2
+sas	          2
+matlab	      1
+looker	      1
+tableau	      1
+pandas	      1
+mysql	      1
+sql	          1
+The analysis shows that skills like Python and SAS are among the most frequently mentioned in the job postings, indicating their importance in the data analyst field.
+
+[
+  {
+    "job_id": 1246069,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "165000.0",
+    "company_name": "Plexus Resource Solutions",
+    "skills": "python"
+  },
+  {
+    "job_id": 1246069,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "165000.0",
+    "company_name": "Plexus Resource Solutions",
+    "skills": "mysql"
+  },
+  {
+    "job_id": 1246069,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "165000.0",
+    "company_name": "Plexus Resource Solutions",
+    "skills": "aws"
+  },
+  {
+    "job_id": 712473,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "165000.0",
+    "company_name": "Get It Recruit - Information Technology",
+    "skills": "sql"
+  },
+  {
+    "job_id": 712473,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "165000.0",
+    "company_name": "Get It Recruit - Information Technology",
+    "skills": "python"
+  },
+  {
+    "job_id": 712473,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "165000.0",
+    "company_name": "Get It Recruit - Information Technology",
+    "skills": "r"
+  },
+  {
+    "job_id": 712473,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "165000.0",
+    "company_name": "Get It Recruit - Information Technology",
+    "skills": "sas"
+  },
+  {
+    "job_id": 712473,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "165000.0",
+    "company_name": "Get It Recruit - Information Technology",
+    "skills": "matlab"
+  },
+  {
+    "job_id": 712473,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "165000.0",
+    "company_name": "Get It Recruit - Information Technology",
+    "skills": "pandas"
+  },
+  {
+    "job_id": 712473,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "165000.0",
+    "company_name": "Get It Recruit - Information Technology",
+    "skills": "tableau"
+  },
+  {
+    "job_id": 712473,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "165000.0",
+    "company_name": "Get It Recruit - Information Technology",
+    "skills": "looker"
+  },
+  {
+    "job_id": 712473,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "165000.0",
+    "company_name": "Get It Recruit - Information Technology",
+    "skills": "sas"
+  },
+  {
+    "job_id": 456042,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "151500.0",
+    "company_name": "Get It Recruit - Healthcare",
+    "skills": "sql"
+  },
+  {
+    "job_id": 456042,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "151500.0",
+    "company_name": "Get It Recruit - Healthcare",
+    "skills": "python"
+  },
+  {
+    "job_id": 456042,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "151500.0",
+    "company_name": "Get It Recruit - Healthcare",
+    "skills": "r"
+  },
+  {
+    "job_id": 405581,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "145000.0",
+    "company_name": "CyberCoders",
+    "skills": "python"
+  },
+  {
+    "job_id": 405581,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "145000.0",
+    "company_name": "CyberCoders",
+    "skills": "java"
+  },
+  {
+    "job_id": 405581,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "145000.0",
+    "company_name": "CyberCoders",
+    "skills": "r"
+  },
+  {
+    "job_id": 405581,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "145000.0",
+    "company_name": "CyberCoders",
+    "skills": "javascript"
+  },
+  {
+    "job_id": 405581,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "145000.0",
+    "company_name": "CyberCoders",
+    "skills": "c++"
+  },
+  {
+    "job_id": 405581,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "145000.0",
+    "company_name": "CyberCoders",
+    "skills": "tableau"
+  },
+  {
+    "job_id": 405581,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "145000.0",
+    "company_name": "CyberCoders",
+    "skills": "power bi"
+  },
+  {
+    "job_id": 405581,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "145000.0",
+    "company_name": "CyberCoders",
+    "skills": "qlik"
+  },
+  {
+    "job_id": 479485,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "145000.0",
+    "company_name": "Level",
+    "skills": "sql"
+  },
+  {
+    "job_id": 479485,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "145000.0",
+    "company_name": "Level",
+    "skills": "python"
+  },
+  {
+    "job_id": 479485,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "145000.0",
+    "company_name": "Level",
+    "skills": "r"
+  },
+  {
+    "job_id": 479485,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "145000.0",
+    "company_name": "Level",
+    "skills": "golang"
+  },
+  {
+    "job_id": 479485,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "145000.0",
+    "company_name": "Level",
+    "skills": "elasticsearch"
+  },
+  {
+    "job_id": 479485,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "145000.0",
+    "company_name": "Level",
+    "skills": "aws"
+  },
+  {
+    "job_id": 479485,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "145000.0",
+    "company_name": "Level",
+    "skills": "bigquery"
+  },
+  {
+    "job_id": 479485,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "145000.0",
+    "company_name": "Level",
+    "skills": "gcp"
+  },
+  {
+    "job_id": 479485,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "145000.0",
+    "company_name": "Level",
+    "skills": "pandas"
+  },
+  {
+    "job_id": 479485,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "145000.0",
+    "company_name": "Level",
+    "skills": "scikit-learn"
+  },
+  {
+    "job_id": 479485,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "145000.0",
+    "company_name": "Level",
+    "skills": "looker"
+  },
+  {
+    "job_id": 479485,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "145000.0",
+    "company_name": "Level",
+    "skills": "kubernetes"
+  },
+  {
+    "job_id": 1090975,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "140500.0",
+    "company_name": "Uber",
+    "skills": "sql"
+  },
+  {
+    "job_id": 1090975,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "140500.0",
+    "company_name": "Uber",
+    "skills": "python"
+  },
+  {
+    "job_id": 1090975,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "140500.0",
+    "company_name": "Uber",
+    "skills": "r"
+  },
+  {
+    "job_id": 1090975,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "140500.0",
+    "company_name": "Uber",
+    "skills": "swift"
+  },
+  {
+    "job_id": 1090975,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "140500.0",
+    "company_name": "Uber",
+    "skills": "excel"
+  },
+  {
+    "job_id": 1090975,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "140500.0",
+    "company_name": "Uber",
+    "skills": "tableau"
+  },
+  {
+    "job_id": 1090975,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "140500.0",
+    "company_name": "Uber",
+    "skills": "looker"
+  },
+  {
+    "job_id": 1482852,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "138500.0",
+    "company_name": "Overmind",
+    "skills": "sql"
+  },
+  {
+    "job_id": 1482852,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "138500.0",
+    "company_name": "Overmind",
+    "skills": "python"
+  },
+  {
+    "job_id": 479965,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "135000.0",
+    "company_name": "InvestM Technology LLC",
+    "skills": "sql"
+  },
+  {
+    "job_id": 479965,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "135000.0",
+    "company_name": "InvestM Technology LLC",
+    "skills": "excel"
+  },
+  {
+    "job_id": 479965,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "135000.0",
+    "company_name": "InvestM Technology LLC",
+    "skills": "power bi"
+  },
+  {
+    "job_id": 1326467,
+    "job_title": "Data Analyst",
+    "salary_year_avg": "135000.0",
+    "company_name": "EPIC Brokers",
+    "skills": "excel"
+  }
+]
+*/
+
